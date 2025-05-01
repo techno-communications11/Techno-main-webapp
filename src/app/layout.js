d9938d3451d71deee94c0f3eb49dd4369464469f
potharajutharun <potharajutharunrana@gmail.com>
2025-05-01 21:10:37 +0530

    second

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,5 @@
 import { Geist, Geist_Mono } from 'next/font/google';
-import '../styles/globals.css';
+import '../Styles/globals.css';
 import Header from '../components/Header';
 import Footer from '@/components/Footer';
 
